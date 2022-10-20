3de19bfe8d0a7a9f9399401d230743098b4a6c09
Isaiah <isababa12@gmail.com>
2022-10-19 17:57:27 -0700

    Feature 7 complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -23,6 +23,7 @@ def redirect_to_receipt_list(request):
 
 
 urlpatterns = [
+    path("accounts/", include("accounts.urls")),
     path("", redirect_to_receipt_list, name="home"),
     path("admin/", admin.site.urls),
     path("receipts/", include("receipts.urls")),
